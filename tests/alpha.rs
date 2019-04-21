85f9621cb9280546981087aa13a08b000e293f9c
sivizius <sivizius@ohai.su>
2019-04-21 14:59:58 +0200

    test alpha.rs to test everything added, should be splitted into unit tests, etc. later

---
+++ tests/alpha.rs
@@ -0,0 +1,76 @@
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+extern crate sucks2;
+use sucks2::
+{
+  assembly::
+  {
+    InstructionSet,
+    x86::
+    {
+      X86,
+    },
+  },
+  formats::
+  {
+    mbr::
+    {
+      MasterBootRecord,
+    },
+    tinaFS::
+    {
+      TinaFS,
+      User,
+    },
+  },
+};
+
+#[test]
+fn main () -> Result<(), &'static str>
+{
+  let mut myTinaFS
+  = TinaFS
+    (
+      4096,
+      8
+    );
+
+  let     sivizius
+  = User
+    (
+      "sivizius",
+      "Hello World!"
+    );
+
+  myTinaFS.addUser ( sivizius ).unwrap();
+
+  let     myCode
+  = X86 ()
+    .label("start")?
+    .add(X86::ax, 42)?
+    .cmp(X86::ax, -42)?;
+
+  let     myAssembly
+  = myCode.compile
+    (
+      InstructionSet::i8086,
+      16,
+      16,
+    ).unwrap();
+
+  let     myMasterBootRecord
+  = MasterBootRecord
+  (
+  );
+
+  myTinaFS.store
+  (
+    "build/alpha.bin",
+    myMasterBootRecord
+  );
+
+  Ok(())
+}
+
+//Hello World
